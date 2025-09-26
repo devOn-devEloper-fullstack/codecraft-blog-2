efc37e94151a26cd7b7edccf83873ca2f1a46ca3
Devon C <devon.cotter42@gmail.com>
2025-09-25 20:30:52 -0400

    Creation of analytics queries and integration with d3 for dashboard capabilities

---
+++ src/lib/server/analytics.ts
@@ -0,0 +1,45 @@
+import { prisma } from './prisma';
+
+//---- Admin Analytics ----//
+
+export async function totalUsersAnalytics() {
+	const totalUsers = await prisma.user.count();
+	return totalUsers;
+}
+
+export async function totalUsersByDate() {
+	const usersByDate = await prisma.user.groupBy({
+		by: ['createdAt'],
+		_count: { id: true },
+		orderBy: { createdAt: 'asc' }
+	});
+	return usersByDate;
+}
+
+export async function totalPostsAnalytics() {
+	const totalPosts = await prisma.posts.count();
+	return totalPosts;
+}
+
+export async function totalPostsByDate() {
+	const postsByDate = await prisma.posts.groupBy({
+		by: ['createdAt'],
+		_count: { id: true },
+		orderBy: { createdAt: 'asc' }
+	});
+	return postsByDate;
+}
+
+export async function totalLikesAnalytics() {
+	const totalLikes = await prisma.postLike.count();
+	return totalLikes;
+}
+
+export async function totalLikesByDate() {
+	const likesByDate = await prisma.postLike.groupBy({
+		by: ['createdAt'],
+		_count: { id: true },
+		orderBy: { createdAt: 'asc' }
+	});
+	return likesByDate;
+}
---
+++ src/routes/posts/(posts)/charts/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import TotalLikesChart from '$lib/components/blocks/dashboard-charts/TotalLikesChart.svelte';
+</script>
+
+<TotalLikesChart />
